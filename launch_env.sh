1f904cb57c10fc1479bd79c85323c865877750d7
kamold <51113520+kamold@users.noreply.github.com>
2026-03-01 15:30:09 +1100

    Force FP

--- launch_env.sh
+++ launch_env.sh
@@ -5,9 +5,13 @@ export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
+export SKIP_FW_QUERY=1
+export FINGERPRINT="VOLKSWAGEN_PASSAT_NMS"
+
 
 if [ -z "$AGNOS_VERSION" ]; then
   export AGNOS_VERSION="10.1"
 fi
 
 export STAGING_ROOT="/data/safe_staging"
+
